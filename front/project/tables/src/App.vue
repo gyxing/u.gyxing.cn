1aa0d414bdced156e6b620eb0b8e0e8a8b9ed713
gyxing <172176234@qq.com>
2019-04-20 02:05:50 +0800

    update

--- front/project/tables/src/App.vue
+++ front/project/tables/src/App.vue
@@ -10,13 +10,16 @@
             >{{item.name}}</span>
         </div>
         <div class="content" v-if="fields.length > 0">
-            <div class="row head">
-                <span>字段</span>
-                <span>说明</span>
-            </div>
-            <div class="row" v-for="(d,i) in fields">
-                <span>{{d.key}}</span>
-                <span>{{d.text}}</span>
+            <div class="table" v-for="(t,i) in fields" :key="'t_'+i">
+                <div class="name" v-if="t.name">{{t.name}}</div>
+                <div class="row head">
+                    <span>字段</span>
+                    <span>说明</span>
+                </div>
+                <div class="row" v-for="(d,k) in t.fields" :key="'f_'+k">
+                    <span>{{d.key}}</span>
+                    <span>{{d.text}}</span>
+                </div>
             </div>
         </div>
     </div>
--- front/project/tables/src/app.js
+++ front/project/tables/src/app.js
@@ -10,26 +10,35 @@ export default {
     },
     computed: {
         tables() {
-            return this.data.map((item, i) => ({
+            let data = this.data;
+            data.sort((a, b) => a.name.localeCompare(b.name) );
+            return data.map((item, i) => ({
                 index: i,
                 ...item
             }));
         }
     },
     mounted() {
-        this.getFields(this.tables[this.tab]);
+        this.getTables(this.tables[this.tab]);
+        document.title = '表结构说明';
     },
     methods: {
         onTab(item, index) {
             this.tab = index;
-            this.getFields(item);
+            this.getTables(item);
         },
-        getFields(item) {
-            if (item.table) {
-                this.fields = Object.keys(item.table).map( d => ({
-                    key: d,
-                    text: item.table[d]
-                }))
+        getTables(item) {
+            if (item.tables) {
+                let tables = item.tables;
+                tables = tables.map( t => ({
+                    name: t.name,
+                    fields: Object.keys(t.fields).map( d => ({
+                        key: d,
+                        text: t.fields[d]
+                    }))
+                }));
+                this.fields = tables;
+                console.log(this.fields)
             }
         }
     }
